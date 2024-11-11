4cefeb256872868625d6cb6eb58e825bdeb9cf1e
Fabian Müller <fabi082002@gmail.com>
2024-11-11 22:13:47 +0100

    klappt

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,15 @@
 <template>
-  <v-app :dark="isDarkTheme">
-    <SettingsMenu />
-    <!-- Weitere Komponenten -->
+  <v-app>
+    <UserProfileForm />
   </v-app>
 </template>
 
 <script>
-import SettingsMenu from './components/SettingsMenü.vue';
+import UserProfileForm from './components/UserProfileForm.vue';
 
 export default {
   components: {
-    SettingsMenu,
-  },
-  data() {
-    return {
-      isDarkTheme: localStorage.getItem("theme") === "Dunkel",
-    };
-  },
-  watch: {
-    // Überwachung des Themes für automatische Aktualisierung
-    isDarkTheme(newValue) {
-      this.$vuetify.theme.dark = newValue;
-    },
-  },
-  created() {
-    // Theme initialisieren
-    this.isDarkTheme = localStorage.getItem("theme") === "Dunkel";
+    UserProfileForm,
   },
 };
 </script>
