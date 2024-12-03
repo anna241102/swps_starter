9ea3fad1ec4f6b10f511743c03c5364d3d8b4b3d
Michl Szameitat <168025379+mszameitatbt@users.noreply.github.com>
2024-12-03 09:10:11 +0100

    Code für Bewertung
    FeedbackForm ist die Component!

---
+++ Codepakete/Bewertung/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <FeedbackForm />
+  </v-app>
+</template>
+
+<script>
+import FeedbackForm from './components/FeedbackForm.vue';
+
+export default {
+  components: {
+    FeedbackForm,
+  },
+};
+</script>
